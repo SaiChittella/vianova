66549ddc2b308b1bd76fb5b47b8d2873d03f599b
Sai Chittella <saichittella30@gmail.com>
2025-04-03 13:00:00 -0700

    Add error logging for authentication processes

--- src/lib/actions/auth.ts
+++ src/lib/actions/auth.ts
@@ -19,7 +19,10 @@ export async function login(formData: FormData) {
 
 	const { error } = await supabase.auth.signInWithPassword(data);
 
-	if (error) redirect("/error");
+	if (error) {
+		console.log("Error: " + JSON.stringify(error, null, 4));
+		redirect("/error");
+	}
 
 	revalidatePath("/", "layout");
 	redirect("/dashboard");
@@ -37,28 +40,28 @@ export async function signup(formData: FormData) {
 
 	const { data: userData, error } = await supabase.auth.signUp(data);
 
-	if (error) redirect("/error")
+	if (error) redirect("/error");
 
 	const restaruantData: InsertRestaurant = {
 		email: formData.get("restaurantEmail") as string,
 		name: formData.get("restaurantName") as string,
-	}
-	
-		const { data: restaurantData, error: insertError } = await supabase
+	};
+
+	const { data: restaurantData, error: insertError } = await supabase
 		.from("restaurants")
 		.insert(restaruantData)
 		.select()
 		.single();
-	
+
 	if (insertError) redirect("/error");
-	
+
 	// TODO: Add email to roles
 	const { error: userError } = await supabase.from("roles").insert({
 		user_id: userData.user?.id,
 		role: "admin",
 		restaurant_id: restaurantData?.id,
 	} as InsertRoles);
-	
+
 	if (userError) redirect("/error");
 
 	revalidatePath("/", "layout");
